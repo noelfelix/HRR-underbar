fed3084224f620657e1f8eb903144a9b7e531fb5
Noel Felix <nfelix83@gmail.com>
2015-11-28 14:52:32 -0800

    Compelted through _.some

--- src/underbar.js
+++ src/underbar.js
@@ -204,13 +204,27 @@
 
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
-    // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function(isEvery, item){
+      if(!isEvery){
+        return false;
+      }
+      if(!iterator){
+        return !!item;
+      }
+      return !!iterator(item);
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    return !_.every(collection, function(item){
+      if(iterator){
+        return !iterator(item);
+      }
+      return !item;
+    });
   };
 
 
